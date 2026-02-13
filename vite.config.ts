cff7176c50678dd57a6b84343594b31eea173a54
PedroBartelle3F <pedrobartelle@3fventures.com.br>
2026-02-13 16:31:13 -0300

    fix: allow host in preview mode

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  preview: {
+    allowedHosts: ["teste-deploy2.3fventure.tech"],
+  },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
